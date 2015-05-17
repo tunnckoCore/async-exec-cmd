f2e10d8741cb6eee549c9461306ce2fffd8b7a87
tunnckoCore <mameto_100@mail.bg>
2015-05-17 04:28:59 +0300

    rewrite tests

--- test.js
+++ test.js
@@ -5,221 +5,176 @@
  * Released under the MIT license.
  */
 
+/* jshint asi:true */
+
 'use strict'
 
 var test = require('assertit')
 var cmd = require('./index')
 
-test('async-exec-cmd:', function () {
-  test('should throw', function () {
-    test('"first argument cant be function" when', function () {
-      test('only function is given (as first argument)', function (done) {
-        function fixture () {
-          cmd(function _cb (err) {
-            /* istanbul ignore next */
-            err = err || ''
-          })
-        }
-        test.throws(fixture, Error)
-        done()
-      })
+test('should throw when only function is given', function (done) {
+  function fixture () {
+    cmd(function _cb (err) {
+      /* istanbul ignore next */
+      err = err || ''
     })
-    test('"should have `callback` (non empty callback)" when', function () {
-      test('only object is given (as first argument)', function (done) {
-        function fixture () {
-          cmd({stdio: 'inherit'})
-        }
-        test.throws(fixture, Error)
-        done()
-      })
-    })
-    test('"should have `callback` (non empty callback)" when', function () {
-      test('only array is given (as first argument)', function (done) {
-        function fixture () {
-          cmd(['--save', 'bluebird'])
-        }
-        test.throws(fixture, Error)
-        done()
-      })
-    })
-    test('"should have `callback` (non empty callback)" when', function () {
-      test('array and object are given', function (done) {
-        function fixture () {
-          cmd(['--save', 'bluebird'], {stdio: 'inherit'})
-        }
-        test.throws(fixture, Error)
-        done()
-      })
-    })
-    test('"should have `callback` (non empty callback)" when', function () {
-      test('asyncExecCmd(cmd, function cb(){}) - callback with empty body', function (done) {
-        function fixture () {
-          cmd('npm install --save is-glob', function _cb () {})
-        }
-        test.throws(fixture, Error)
-        done()
-      })
+  }
+  test.throws(fixture, Error)
+  done()
+})
+
+test('should throw "should have `callback` (non empty callback)"', function () {
+  test('when only object is given', function (done) {
+    function fixture () {
+      cmd({stdio: 'inherit'})
+    }
+    test.throws(fixture, Error)
+    done()
+  })
+  test('when array is given', function (done) {
+    function fixture () {
+      cmd(['--save', 'bluebird'])
+    }
+    test.throws(fixture, Error)
+    done()
+  })
+  test('when array and object are given', function (done) {
+    function fixture () {
+      cmd(['--save', 'bluebird'], {stdio: 'inherit'})
+    }
+    test.throws(fixture, Error)
+    done()
+  })
+  test('when callback with empty body', function (done) {
+    function fixture () {
+      cmd('npm install --save is-glob', function _cb () {})
+    }
+    test.throws(fixture, Error)
+    done()
+  })
+})
+
+test('should have Error in callback', function () {
+  test('when process could not be spawned', function (done) {
+    cmd('jkdshfkj4hkjh435', function _cb (err) {
+      test.ok(err instanceof Error)
+      done()
     })
   })
+})
 
-  test('should callback(err) recieve', function () {
-    test('from spawn `error` event because', function () {
-      test('the process could not be spawned', function (done) {
-        cmd('jkdshfkj4hkjh435', function _cb (err) {
-          test.ok(err instanceof Error)
-          done()
-        })
-      })
-    })
-    test('TypeError "expect `cmd` be string" when', function () {
-      test('object and callback are given', function (done) {
-        cmd({stdio: 'inherit'}, function _cb (err) {
-          test.ok(err instanceof TypeError)
-          done()
-        })
-      })
-    })
-    test('TypeError "expect `cmd` be string" when', function () {
-      test('array and callback are given', function (done) {
-        cmd(['--save', 'bluebird'], function _cb (err) {
-          test.ok(err instanceof TypeError)
-          done()
-        })
-      })
-    })
-    test('TypeError "expect `cmd` be string" when', function () {
-      test('array, object and callback are given', function (done) {
-        cmd(['--save', 'bluebird'], {stdio: 'inherit'}, function _cb (err) {
-          test.ok(err instanceof TypeError)
-          done()
-        })
-      })
-    })
-    test('TypeError "expect `cmd` be string" when', function () {
-      test('object, array and callback are given', function (done) {
-        cmd({stdio: 'inherit'}, ['--save', 'bluebird'], function _cb (err) {
-          test.ok(err instanceof TypeError)
-          done()
-        })
-      })
+test('should have TypeError in callback', function () {
+  test('when object and callback given', function (done) {
+    cmd({stdio: 'inherit'}, function _cb (err) {
+      test.ok(err instanceof TypeError)
+      done()
     })
   })
+  test('when array and callback are given', function (done) {
+    cmd(['--save', 'bluebird'], function _cb (err) {
+      test.ok(err instanceof TypeError)
+      done()
+    })
+  })
+  test('when array, object and callback are given', function (done) {
+    cmd(['--save', 'bluebird'], {stdio: 'inherit'}, function _cb (err) {
+      test.ok(err instanceof TypeError)
+      done()
+    })
+  })
+  test('when object, array and callback are given', function (done) {
+    cmd({stdio: 'inherit'}, ['--save', 'bluebird'], function _cb (err) {
+      test.ok(err instanceof TypeError)
+      done()
+    })
+  })
+})
 
-  test('should work properly', function () {
-    test('should return `stream` when asyncExecCmd() is executed', function () {
-      test('and should have `.kill` method', function (done) {
-        var cp = cmd('npm version', function _cb (err) {
-          /* istanbul ignore next */
-          err = err || ''
-        })
-        test.equal(typeof cp.kill, 'function')
-        done()
-      })
+test('should return `stream` and have `.kill` method when executed', function (done) {
+  var cp = cmd('npm version', function _cb (err) {
+    /* istanbul ignore next */
+    err = err || ''
+  })
+  test.equal(typeof cp.kill, 'function')
+  done()
+})
+
+test('should have empty res and err null when opts.stdio is inherit', function (done) {
+  cmd('npm install --save is-glob', {stdio: 'inherit'}, function _cb (err, res, code) {
+    test.equal(err, null)
+    test.equal(code, 0) // status code
+    test.equal(typeof res, 'string') // response
+    test.equal(res.length, 0)
+    done()
+  })
+})
+
+function cmdErrorTest (err, res, code, buf, done) {
+  test.equal(res, undefined)
+  test.equal(buf, undefined)
+  test.equal(code, 1)
+  test.equal(err.status, 1)
+  test.equal(err.name, 'CommandError')
+  done()
+}
+
+test('should have CommandError in callback when', function () {
+  test('asyncExecCmd("npm", cb)', function (done) {
+    cmd('npm', function _cb (err, res, code, buf) {
+      cmdErrorTest(err, res, code, buf, done)
     })
-    test('when opts.stdio is "inherit"', function () {
-      test('and should recieve res === "", code === 0, err === null', function (done) {
-        cmd('npm install --save is-glob', {stdio: 'inherit'}, function _cb (err, res, code) {
-          test.equal(err, null)
-          test.equal(code, 0) // status code
-          test.equal(typeof res, 'string') // response
-          test.equal(res.length, 0)
-          done()
-        })
-      })
+  })
+  test('asyncExecCmd("npm", opts, cb)', function (done) {
+    cmd('npm', {stdio: [null, null, null]}, function _cb (err, res, code, buf) {
+      cmdErrorTest(err, res, code, buf, done)
     })
-    test("when asyncExecCmd('npm', [args[, opts]], cb)", function () {
-      test('and handle CommandError when', function () {
-        test("asyncExecCmd('npm', cb)", function (done) {
-          cmd('npm', function _cb (err, res, code, buffer) {
-            test.equal(res, undefined)
-            test.equal(buffer, undefined)
-            test.equal(code, 1)
-            test.equal(err.status, 1)
-            test.equal(err.name, 'CommandError')
-            done()
-          })
-        })
-        test("asyncExecCmd('npm', opts, cb)", function (done) {
-          cmd('npm', {stdio: [null, null, null]},
-          function _cb (err, res, code, buffer) {
-            test.equal(res, undefined)
-            test.equal(buffer, undefined)
-            test.equal(code, 1)
-            test.equal(err.status, 1)
-            test.equal(err.name, 'CommandError')
-            done()
-          })
-        })
-      })
-      test('and callback error should be `null` and should have response when', function () {
-        test("asyncExecCmd('npm', ['i', '--save', 'is-glob'], cb)", function (done) {
-          cmd('npm', ['install', '--save', 'is-glob'],
-          function _cb (err, res, code) {
-            test.equal(err, null)
-            test.equal(code, 0)
-            test.equal(typeof res, 'string')
-            test.ok(res.length > 0)
-            done()
-          })
-        })
-        test("asyncExecCmd('npm', ['uni', '--save', 'is-glob'], opts, cb)", function (done) {
-          cmd('npm', ['uninstall', '--save', 'is-glob'], {stdio: [null, null, null]},
-          function _cb (err, res, code) {
-            test.equal(err, null)
-            test.equal(code, 0)
-            test.equal(typeof res, 'string')
-            test.ok(res.length > 0)
-            done()
-          })
-        })
-      })
+  })
+})
+
+test('should accept single command and arguments in array', function () {
+  test("asyncExecCmd('npm', ['i', '--save', 'is-glob'], cb)", function (done) {
+    cmd('npm', ['install', '--save', 'is-glob'],
+    function _cb (err, res, code) {
+      test.equal(err, null)
+      test.equal(code, 0)
+      test.equal(typeof res, 'string')
+      test.ok(res.length > 0)
+      done()
     })
-    test("when asyncExecCmd('npm subcommdand', [args[, opts]], cb)", function () {
-      test('and handle CommandError when', function () {
-        test("asyncExecCmd('npm unknown', cb)", function (done) {
-          cmd('npm unknown', function _cb (err, res, code, buffer) {
-            test.equal(res, undefined)
-            test.equal(buffer, undefined)
-            test.equal(code, 1)
-            test.equal(err.status, 1)
-            test.equal(err.name, 'CommandError')
-            done()
-          })
-        })
-        test("asyncExecCmd('npm unknown', opts, cb)", function (done) {
-          cmd('npm unknown', {stdio: [null, null, null]},
-          function _cb (err, res, code, buffer) {
-            test.equal(res, undefined)
-            test.equal(buffer, undefined)
-            test.equal(code, 1)
-            test.equal(err.status, 1)
-            test.equal(err.name, 'CommandError')
-            done()
-          })
-        })
-      })
-      test('and callback error should be `null` and should have response when', function () {
-        test("asyncExecCmd('npm i', ['--save', 'is-glob'], cb)", function (done) {
-          cmd('npm install', ['--save', 'is-glob'],
-          function _cb (err, res, code) {
-            test.equal(err, null)
-            test.equal(code, 0)
-            test.equal(typeof res, 'string')
-            test.ok(res.length > 0)
-            done()
-          })
-        })
-        test("asyncExecCmd('npm uni', ['--save', 'is-glob'], opts, cb)", function (done) {
-          cmd('npm uninstall', ['--save', 'is-glob'], {stdio: [null, null, null]},
-          function _cb (err, res, code) {
-            test.equal(err, null)
-            test.equal(code, 0)
-            test.equal(typeof res, 'string')
-            test.ok(res.length > 0)
-            done()
-          })
-        })
-      })
+  })
+  test("asyncExecCmd('npm', ['uni', '--save', 'is-glob'], opts, cb)", function (done) {
+    cmd('npm', ['uninstall', '--save', 'is-glob'], {stdio: [null, null, null]},
+    function _cb (err, res, code) {
+      test.equal(err, null)
+      test.equal(code, 0)
+      test.equal(typeof res, 'string')
+      test.ok(res.length > 0)
+      done()
+    })
+  })
+})
+
+test('should accept multiple words in `cmd` and `args` in array', function () {
+  test('should recieve CommandError if unknown subcommand given', function (done) {
+    cmd('npm unknown', function _cb (err, res, code, buffer) {
+      test.equal(res, undefined)
+      test.equal(buffer, undefined)
+      test.equal(code, 1)
+      test.equal(err.status, 1)
+      test.equal(err.name, 'CommandError')
+      done()
+    })
+  })
+  test('should work when asyncExecCmd("npm install", ["node-foo", "node-bar"], opts, cb)', function (done) {
+    var opts = {stdio: [null, null, null]}
+    var args = ['node-foo', 'node-bar']
+
+    cmd('npm install', args, opts, function _cb (err, res, code) {
+      test.equal(err, null)
+      test.equal(code, 0)
+      test.equal(typeof res, 'string')
+      test.ok(res.length > 0)
+      done()
     })
   })
 })
